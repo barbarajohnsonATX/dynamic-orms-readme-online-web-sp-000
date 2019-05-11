3c1d8cec10c8f00cb85232f625d2ab743ed3997f
Barbara Johnson <johnson.barbara@gmail.com>
2019-05-11 03:25:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,7 +33,9 @@ class Song
 
   def save
     sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+    binding.pry
     DB[:conn].execute(sql)
+    binding.pry
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
 
